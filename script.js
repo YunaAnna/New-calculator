e04bd82794fec2948b8e2093d5243ea2693bc21c
YunaAnna <119411093+YunaAnna@users.noreply.github.com>
2023-03-28 12:42:55 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const input1 = document.querySelector('.js-teksts');
 const plusButton = document.querySelector('.js-plus')
+const Add =("skaitlis1, skaitlis2") {
+    return skaitlis1 + skaitlis2
+}
 
+
+const Substract =(skaitlis1,skaitlis2) => {
+    return skaitlis1 - skaitlis2
+}
+
+const Substract =(skaitlis1,skaitlis2) => {
+    return skaitlis1 - skaitlis2
+}
+
+const Multiply =(skaitlis1,skaitlis2) => {
+    return skaitlis1 * skaitlis2
+}
+
+const Divide =(skaitlis1,skaitlis2) => {
+    return skaitlis1 / skaitlis2
+}
+
+plusButton.addEventListener('click', () =>{
+    console.log("Mani nospieda")
+})
 
 console.log(input1);
-console.log(plusButton)
+
+console.log(Add(22,13))
